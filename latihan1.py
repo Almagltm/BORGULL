0456ec26f72ed5a0cffe38460c89d31ac196c9ce
Almagltm <almamurael@gmail.com>
2024-09-23 23:11:53 +0700

    learning at home part 1

---
+++ latihan1.py
@@ -0,0 +1,5 @@
+saldo_awal = input('berapa saldo awalmu:' )
+Deposit = input('mau deposit berapa?' )
+saldo_akhir = float(saldo_awal) +  float(Deposit) 
+
+print( 'saldo akhirmu adalah : ' + 'Rp ' + str(saldo_akhir) )
